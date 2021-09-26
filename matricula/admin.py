c9f0c2bf3c2ae7d07ae26b35bf3247bbcc33e740
abel <aamarquele@gmail.com>
2021-09-26 14:57:54 +0200

    Transferido o codigo da Api mypai

--- matricula/admin.py
+++ matricula/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from .models import Matricula, Matricula_Turma
 
-# Register your models here.
-
 admin.site.register(Matricula)
 admin.site.register(Matricula_Turma)
 
